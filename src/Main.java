364ce53142c5e6b9e61d89551e59f417ad85ae6c
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-11-28 12:10:58 +0100

    Update Main.java
    update

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,17 @@
 //Creare una classe Builder che attraverso il costruttore e dei metodi specifici (tranne nome e cognome,
 // gli altri campi sono opzionali) per costruire l'oggetto Person.
 //Creare due oggetti Person e stamparli a video
-
+//Design patterns - Builder
 public class Main {
     public static void main(String[] args) {
         Builder builder = new Builder("MARIO","ROSSI");
         Person persona =builder.build();
         System.out.println(persona);
 
-        Person persona1 = new Person("MARCO","GATTI",24,"Via Roma");
+        Builder builder1 = new Builder("Franco","Ganni");
+        builder1.setAge(33);
+        builder1.setAddress("via roma");
+        Person persona1 = builder1.build();
         System.out.println(persona1);
     }
 }
