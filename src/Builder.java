4f88ccf98682e3ee991db2de56faf9ad6a5c69fd
simonecordisco <147503322+simonecordisco@users.noreply.github.com>
2023-11-28 12:10:39 +0100

    Update Builder.java
    update1

--- src/Builder.java
+++ src/Builder.java
@@ -1,13 +1,34 @@
 public class Builder {
-    private String firstName;
-    private String lastName;
-    private int age;
-    private String address;
+    public String firstName;
+    public  String lastName;
+    public  int age;
+    public  String address;
+    public String getLastName() {
+        return lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public int getAge() {
+        return age;
+    }
 
-    public Builder(String lastName,String firstName) {
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Builder(String lastName, String firstName) {
     this.lastName=lastName;
     this.firstName=firstName;
-    }
+         }
 
     public Builder setfirstName(String firstName) {
         this.firstName = firstName;
@@ -18,7 +39,19 @@ public class Builder {
         this.lastName = lastName;
         return this;
     }
+
+    @Override
+    public String toString() {
+        return "Builder{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
     public Person build(){
-        return new Person(firstName,lastName,age,address);
+        return new Person(this);
     }
 }
+
